012604efd7fb0de9eeab6a7e6f9cb4578ec68f08
Yoshihisa Sakuraba <ys80276@partner.technosystems.io>
2022-06-07 15:53:46 +0900

    日本語化

--- ルーティング/静的ルーティング/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("Express");
-var app = express();
-
-var routes = require("./routes.js");
-
-//both index.js and things.js should be in same directory
-app.use("/routes", routes);
-
-app.listen(3000);
